78bbfbc343c0ebd7e9cbc2dcfc7a6ed1ede2994f
Apler <zjuapler@gmail.com>
2018-07-19 00:00:08 +0800

    User db

---
+++ src/main/java/com/apler/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.apler.dao;
+
+import com.apler.entity.user.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserDao extends CrudRepository<User, Long> {
+    @Query(value = "select * from user where phone = ?1", nativeQuery = true)
+    User findByPhone(String phone);
+}
---
+++ src/main/java/com/apler/service/UserService.java
@@ -0,0 +1,21 @@
+package com.apler.service;
+
+import com.apler.dao.UserDao;
+import com.apler.entity.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserDao userDao;
+
+    public Iterable<User> getAllUser(){
+        return userDao.findAll();
+    }
+
+    public User getUser(@PathVariable String phone){
+        return userDao.findByPhone(phone);
+    }
+}
